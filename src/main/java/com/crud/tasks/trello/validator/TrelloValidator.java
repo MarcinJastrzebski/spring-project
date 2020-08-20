eb3c2a4aa5658ff29661916150bbd85ba6cca0d3
Marcin Jastrzębski <marcin.jastrzebski@outlook.com>
2020-08-20 22:52:33 +0200

    next heroku

--- src/main/java/com/crud/tasks/trello/validator/TrelloValidator.java
+++ src/main/java/com/crud/tasks/trello/validator/TrelloValidator.java
@@ -2,6 +2,7 @@ package com.crud.tasks.trello.validator;
 
 import com.crud.tasks.domain.TrelloBoard;
 import com.crud.tasks.domain.TrelloCard;
+import com.crud.tasks.trello.facade.TrelloFacade;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -11,7 +12,7 @@ import java.util.stream.Collectors;
 
 @Component
 public class TrelloValidator {
-    private static final Logger LOGGER = LoggerFactory.getLogger(TrelloValidator.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(TrelloFacade.class);
 
     public void validateCard(final TrelloCard trelloCard) {
         if (trelloCard.getName().contains("test")) {
@@ -24,7 +25,7 @@ public class TrelloValidator {
     public List<TrelloBoard> validateTrelloBoards(final List<TrelloBoard> trelloBoards) {
         LOGGER.info("Starting filtering boards...");
         List<TrelloBoard> filteredBoards = trelloBoards.stream()
-                .filter(trelloBoard -> trelloBoard.getName().equalsIgnoreCase("test"))
+                .filter(trelloBoard -> !trelloBoard.getName().equalsIgnoreCase("test"))
                 .collect(Collectors.toList());
         LOGGER.info("Boards have been filtered. Current list size: " + filteredBoards.size());
         return filteredBoards;
